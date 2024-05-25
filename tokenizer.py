b3499baca8941db490720243cf46e86dfa342b4f
Leonardo Louis Parilla Gutierrez IV <65400514+ssaidwhotho@users.noreply.github.com>
2024-05-25 14:58:01 -0700

    HTML now, gets fields

--- tokenizer.py
+++ tokenizer.py
@@ -2,6 +2,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from bs4 import BeautifulSoup
 
+
 def is_alphanumeric(token: str) -> bool:
     # Check if the token is alphanumeric
     for char in token:
@@ -10,30 +11,54 @@ def is_alphanumeric(token: str) -> bool:
     return True
 
 
-def get_tokens(document) -> list:
-    try:
-        current_doc = document['content']  # Copy current document
-        soup = BeautifulSoup(current_doc, 'xml')  # Parse the document with BeautifulSoup
-        for script in soup(["script", "style"]):
-            script.extract()
-        clean_text = soup.get_text()  # Get the text from the document
-
-        return tokenize(clean_text)
-    except Exception as e:
-        print(document['content'])
-        print(f"Error: {e}")
-        return []
-
-
 def tokenize(text: str) -> list:
+    """
+    This function will take in a string and return a list of tokens.
+    :param text: str
+    :return: list of tokens
+    """
     tokens = word_tokenize(text)
 
     tokens = [token.lower() for token in tokens if
               is_alphanumeric(token) or (
-                          is_alphanumeric(token.replace(".", "")) and len(token) > 1)]
+                      is_alphanumeric(token.replace(".", "")) and len(token) > 1)]
     p_stemmer = PorterStemmer()
     tokens = [p_stemmer.stem(word) for word in tokens]  # Stem using PorterStemmer
-    # bigram_tokens = []
-    # for i in range(len(tokens) - 1):
-    #     bigram_tokens.append(tokens[i] + " " + tokens[i + 1])
-    return tokens  # , bigram_tokens
+    return tokens
+
+
+def get_tokens(document) -> dict and list[tuple]:
+    """
+    This function will take in the document, which is the json, and then will
+    return the tokens and a dictionary of the tokens with bools marking if they are in the title, bold,
+    """
+    try:
+        current_doc = document['content']  # Copy current document
+        soup = BeautifulSoup(current_doc, 'html.parser')  # Parse the document
+        for script in soup(["script", "style"]):
+            script.extract()
+        # find all hyperlinks and their anchor text
+        hyperlinks = []
+        for link in soup.find_all('a'):
+            hyperlinks.append((link.get('href'), link.text))
+        # find title and bold tags
+        titles = soup.find('title') if soup.find('title') else []
+        bold = soup.find('strong') if soup.find('strong') else []
+        bold2 = soup.find('b') if soup.find('b') else []
+        clean_text = soup.get_text()  # Get the text from the document
+        tokens = tokenize(clean_text)  # Tokenize the text
+        token_dict = {}  # Create a dictionary to store the tokens
+        pos = 1
+        for token in tokens:
+            # token: (frequency, title?, bold?, positions)
+            if token in token_dict:
+                token_dict[token][0] += 1
+                token_dict[token][3].append(pos)
+            else:
+                token_dict[token] = [1, token in titles, token in bold or token in bold2, [pos]]
+            pos += 1
+        return token_dict, hyperlinks
+    except Exception as e:
+        print(document['content'])
+        print(f"Error: {e}")
+        return []
